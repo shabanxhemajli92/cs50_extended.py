49306d0ef62cfdd4b4852da914cd8d9ad27a7ef7
Shaban <shabanxhemajli@live.com>
2022-11-03 15:38:00 +0100

    quick identification work

---
+++ id.py
@@ -0,0 +1,15 @@
+def main():
+    student = get_Person()
+    print(f"{student['name']} {student['Last Name']} was born on {student['birth date']} in {student['where were you born']} and lives in {student['Where do you live']}")
+
+def get_Person():
+    
+    name=input("Name: ")
+    last_name=input("Last Name: ")
+    Birth_date=input("Date of birth: ")
+    Birth_place=input("What is your birth place: ")
+    house=input("Where do you live: ")
+    return {"name":name,"Last Name":last_name,"birth date":Birth_date,"where were you born":Birth_place,"Where do you live":house}
+
+if __name__== "__main__":
+    main()
