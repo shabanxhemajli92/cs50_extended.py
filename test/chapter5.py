887f466df159514a8b74349a6e73aa9ed7b666f1
Shaban <shabanxhemajli@live.com>
2022-11-08 14:21:34 +0100

    some more work

---
+++ test/chapter5.py
@@ -0,0 +1,32 @@
+# minutes=int(input("How many minutes: "))
+# hours= minutes // 60
+# print(hours)
+
+# remainder= minutes - hours * 60
+# print(remainder)
+
+# x = int(input("first number: "))
+# y = int(input("second number: "))
+# if x % y == 0:
+#     print("x is / by y")
+# else:
+#     print("x is not/ by y")
+
+#x = int(input("first number: "))
+#y = int(input("second number: "))
+# if x % 2 ==0:
+#     print("num is even","+ it is also",True)
+# else:
+#     print("num is odd","+ it is also",False)
+# if 0 < x < 10:
+#         print('x is a positive single-digit number.')
+# else:
+#         print('x is a positive double-digit number.')
+
+def countdown(n):
+    if n <= 0:
+        print('Blastoff!')
+    else:
+        print(n)
+        countdown(n-1)
+print(countdown(8))    
--- test/test.py
+++ test/test.py
@@ -1,7 +1,8 @@
 import unittest
 from student import get_student
 
+
 class TestDeleteFile(unittest.TestCase):
 
     def test_get_student(self):
-       self.assertEqual(("John"),"John")
+        self.assertEqual(("John"), "John")
