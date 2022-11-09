80096c447688a5952fefe7e34df562bf03f5528e
Shaban <shabanxhemajli@live.com>
2022-11-09 20:13:38 +0100

    some more late night work

--- test/class_nov7.py
+++ test/class_nov7.py
@@ -1,23 +1,35 @@
 class Student:
 
-    def __init__(self, name, house):
+    def __init__(self, name, house,patronus):
         if not name:
             raise ValueError("Missing name")
         if house not in ["Grifyndor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid hose")
+            raise ValueError("Invalid house")
         self.name = name
         self.house = house
+        self.patronus = patronus
+    
+    def __str__(self) -> str:
+        return f"{self.name} from {self.house}"
 
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return "#" 
+            case "Otter":
+                return "?"
+            case "Jack"          
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(student)
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return Student(name, house)
+    patronus=input("Patronus: ")
+    return Student(name, house,patronus)
 
 
 if __name__ == "__main__":
